9594b98f56ca8199c93e3184c646b1b0b788a232
jirryzhang <jirryzhang@qq.com>
2017-07-08 17:48:01 +0800

    上位机波形绘制通信部分的注释和优化
    匿名四轴上位机波形绘制通信部分的注释和优化

--- Framework/applications/tasks_cmcontrol.c
+++ Framework/applications/tasks_cmcontrol.c
@@ -49,18 +49,20 @@ WorkState_e GetWorkState()
 }
 /*2ms定时任务*/
 
-extern float ZGyroModuleAngle;
+extern float ZGyroModuleAngle;//Yaw轴角度
 float ZGyroModuleAngleMAX;
 float ZGyroModuleAngleMIN;
 extern float yawRealAngle;
 extern uint8_t GYRO_RESETED;
-extern float pitchRealAngle;
-extern float gYroZs;
+extern float pitchRealAngle;//Pitch轴角度
+extern float gYroZs;////Yaw轴角速度
 extern float yawAngleTarget;
 extern float yawRealAngle;
 
 extern uint8_t JUDGE_STATE;
 //*********debug by ZY*********
+
+/*
 typedef struct{
 	uint16_t head;
 	uint8_t id;
@@ -69,10 +71,22 @@ typedef struct{
 	float dataYaw;
 	uint8_t checkSum;
 }data_to_PC;
+*/
 
 
-uint8_t data_send_to_PC[17];
-data_to_PC my_data_to_PC;
+uint8_t data_send_to_PC[17];//要发送给上位机的报文
+//data_to_PC my_data_to_PC;
+
+/*
+发送给上位机的数据帧定义
+@前2个字节为帧头0xAAAA
+@第3个字节为帧ID，应设置为0xF1~0xFA中的一个
+@第4个字节为报文数据长度(dlc)
+@第5个字节开始到第5+dlc-1个字节为要传输的数据内容段，每个数据场为高字节在前，地字节在后
+@第5+dlc个字节为CheckSum,为第1个字节到第5+dlc-1个字节所有字节的值相加后，保留结果的低八位作为CheckSum
+@我们需要传输3个float数据，pitchRealAngle(Pitch),ZGyroModuleAngle(Yaw), gYroZs(Speed),dlc应该为3*4 bytes=12 bytes
+@所以整个报文长度为5+12=17个字节
+*/
 void send_data_to_PC(UART_HandleTypeDef *huart,float zyPitch,float zyYaw,float zySpd)
 {
 //	my_data_to_PC.head=0xAAAA;
@@ -95,10 +109,15 @@ void send_data_to_PC(UART_HandleTypeDef *huart,float zyPitch,float zyYaw,float z
 	uint8_t * pTemp;
 	int i;
 	data_send_to_PC[0]=0xAA;
-	data_send_to_PC[1]=0xAA;
-	data_send_to_PC[2]=0xF1;
-	data_send_to_PC[3]=12;
-	pTemp=(uint8_t *)&zyPitch;
+	data_send_to_PC[1]=0xAA;//前两个字节为帧头0xAA
+	data_send_to_PC[2]=0xF1;//第3个字节为帧ID，设为0xF1
+	data_send_to_PC[3]=12;//第4个字节为数据长度dlc，需要传输3个float数据(3*4 bytes=12 bytes)
+	pTemp=(uint8_t *)&zyPitch;//将数据场首指针转换为uint8_t型指针，方便后续操作
+	
+	/*第一个float数据Pitch轴角度，传输时要将高位放在前面，低位放在后面
+	STM32是小端存储(Little Endian),高位数据储存在高地址中。
+	所以需要如下的转换，将高位数据移到数据帧的前面来。其他两个数据Yaw和速度Spd用同样方法处理
+	*/
 	for(i=0;i<4;i++)
 	{
 		 data_send_to_PC[4+i]=pTemp[3-i];
@@ -116,13 +135,13 @@ void send_data_to_PC(UART_HandleTypeDef *huart,float zyPitch,float zyYaw,float z
 		 data_send_to_PC[12+i]=pTemp[3-i];
 	}
 	
-	data_send_to_PC[16]=0;
+	data_send_to_PC[16]=0;//第17个字节为本帧报文的CheckSum，按照前述CheckSum的要求求和计算即可
 	for(i=0;i<16;i++)
 	{
 		 data_send_to_PC[16]+=data_send_to_PC[i];
 	}
 	
-	HAL_UART_Transmit(huart,data_send_to_PC,17,1000);
+	HAL_UART_Transmit(huart,data_send_to_PC,17,1000);//向上位机发送本帧报文，共17个字节
 }
 
 //*********debug by ZY*********
