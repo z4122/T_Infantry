c239629f5955d95008cce8156d913496b26ac25e
邱志康 <zhikangqiu@163.com>
2017-07-11 10:34:40 +0800

    新增拨盘电机Task

--- Framework/applications/tasks_platemotor.h
+++ Framework/applications/tasks_platemotor.h
@@ -19,6 +19,57 @@
 #include "tasks_timed.h"
 #include "pid_regulator.h"
 
-void ShootOneBullet(PID_Regulator_t *ShootMotorPositionPID);
+#define OneShoot (722)
+#define PID_SHOOT_MOTOR_SPEED      (30)
 
+#define SHOOT_MOTOR_POSITION_PID_DEFAULT \
+{\
+	0,\
+	0,\
+	{0,0},\
+	7.5f,\
+	0.05f,\
+	0.6f,\
+	0,\
+	0,\
+	0,\
+	4900,\
+	3500,\
+	1500,\
+	0,\
+	1000,\
+	0,\
+	0,\
+	0,\
+	&PID_Calc,\
+	&PID_Reset,\
+}\
+
+#define SHOOT_MOTOR_SPEED_PID_DEFAULT \
+{\
+	0,\
+	0,\
+	{0,0},\
+	50.0f,\
+	0.5f,\
+	0.0f,\
+	0,\
+	0,\
+	0,\
+	1000,\
+	200,\
+	100,\
+	0,\
+	4950,\
+	0,\
+	0,\
+	0,\
+	&PID_Calc,\
+	&PID_Reset,\
+}\
+
+
+void PlateMotorTask(void const * argument);
+void ShootOneBullet(void);
+int32_t GetQuadEncoderDiff(void);
 #endif
